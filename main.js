c48013f7390e9c0d02ae30555a39005c960611c5
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-03-28 15:55:55 -0400

    Added difficulty method and tweaked guess method further. Next step is to test if the letter will appear correctly on screen once user input guessed right.

--- main.js
+++ main.js
@@ -15,25 +15,52 @@ const startBtn = document.querySelector(".start-btn");
 const flexContainer = document.querySelector(".flex-container");
 const instructionsEl = document.querySelector(".instructions");
 const userInput = document.querySelector(".user-input");
+const dashesContainer = document.querySelector(".dashes-container");
+const difficulty = document.querySelector("#difficulty-container");
 
 
 startBtn.addEventListener('click', function (event) {
     if (event.target) {
         startBtn.style.visibility = 'hidden';
 
-        instructionsEl.textContent = 'Player turn: enter a letter';
+        instructionsEl.textContent = 'Choose your difficulty';
         instructionsEl.setAttribute('class', 'instructions-design');
 
+        chooseDifficulty();
+        generateBlocks(); //put this inside an addeventlistener in the choosedifficulty method
 
-        generateBlocks();
     }
 });
+
+function chooseDifficulty()
+{
+    const easyBtn = document.createElement('button');
+    const mediumBtn = document.createElement('button');
+    const hardBtn = document.createElement('button');
+
+    easyBtn.innerText = 'EASY';
+    mediumBtn.innerText = 'MEDIUM';
+    hardBtn.innerHTML = 'HARD';
+
+    difficulty.appendChild(easyBtn);
+    difficulty.appendChild(mediumBtn);
+    difficulty.appendChild(hardBtn);
+
+    difficulty.addEventListener('click',function(event)
+    {
+        //depedning on the innertext of the button, generateblocks and randomwords depending on the difficulty array chosen
+    })
+
+
+
+}
 
 //generate letter blocks
 function generateBlocks() {
     //generate the letters
     flexContainer.classList.remove("hidden");
 
+    //generate letter buttons
     for(let i = 65; i < 91; i++)
     {
         const letter = document.createElement('button');
@@ -57,7 +84,20 @@ function generateRandomWord() {
         word = randomWords[random];
     }
 
+    console.log(word.length);
+
+    //generate dashes depending on number of random words
+    for(let j = 0; j < word.length; j++)
+    {
+        const dashes = document.createElement('div');
+        dashes.classList.add('dotted-lines');
+        dashesContainer.appendChild(dashes);
+        dashesContainer.children[j].textContent = word[j];
+        dashesContainer.children[j].style.color = 'white';
+    }
+
     console.log(word);
+    console.log(dashesContainer)
     guess(word);
 }
 
@@ -66,8 +106,6 @@ let winCount = 0;
 function guess(word)
 {
     const correctWord = word.split("");
-    // let arrayof = [];
-    // arrayof.splice()
 
     console.log(correctWord);
 
@@ -76,9 +114,19 @@ function guess(word)
         console.log(event.target.innerText);
         if(correctWord.includes(event.target.innerText.toLowerCase()))
         {
-            if(event.target.innerText.toLowerCase().length) //check if a letter is in an array more than once before disabling the button
+            //if(event.target.innerText.toLowerCase().length) //check if a letter is in an array more than once before disabling the button
             event.target.disabled = true;
-
+            const displayLetter = document.createElement('p');
+            displayLetter.textContent = event.target.innerText;
+            displayLetter.classList.add('letter-design');
+            //TODO add html elements. Trying to see if the letter in the dashes container is equal to the index of the correct word and replacing the inner html
+            for(let i = 0; i < correctWord.length; i++)
+            {
+                if(dashesContainer.children[i].innerHTML === correctWord[i])
+                {
+                    dashesContainer.children[i].innerHTML = displayLetter.textContent;
+                }
+            }
             winCount += 1;
             checkIfWon(correctWord);
             checkIfLose();
@@ -97,6 +145,7 @@ function checkIfWon(correctWord)
 {
     if(winCount === correctWord.length)
     {
+        //TODO add html elements
         console.log("You won!")
     }
 }
@@ -105,6 +154,7 @@ function checkIfLose()
 {
     if(userHealth == 0)
     {
+        //TODO add html elements
         console.log("You lost")
     }
 }
